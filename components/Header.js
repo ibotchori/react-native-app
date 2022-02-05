3407daea0af83b3bd97d4600523c88ea7a707ebf
Irakli <koregm@gmail.com>
2022-02-05 23:10:33 +0400

    Add header

---
+++ components/Header.js
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Header({title}) {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+}
+
+
+// if no props received, component takes this
+Header.defaultProps = {
+    title: 'Shopping List'
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: "darkslateblue",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 23,
+    textAlign: "center",
+  },
+});
