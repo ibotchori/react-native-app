2216d5481c2c3df2b603403210338df7de46ac04
Irakli <koregm@gmail.com>
2022-02-05 23:49:50 +0400

    Add ListItem component

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import Header from "./components/Header";
+import ListItem from "./components/ListItem";
 
 export default function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      text: "Milk",
+    },
+    {
+      id: 2,
+      text: "Eggs",
+    },
+    {
+      id: 3,
+      text: "Bread",
+    },
+    {
+      id: 4,
+      text: "Juice",
+    },
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <ListItem item={item} />}
+      />
     </View>
   );
 }
@@ -14,5 +38,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 40,
   },
 });
