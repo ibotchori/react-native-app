2f46628cd8691b0451dc3539141761c6ce6060e0
Irakli <koregm@gmail.com>
2022-02-07 14:09:19 +0400

    Get data from mongo database

--- App.js
+++ App.js
@@ -1,29 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Alert, View, FlatList } from "react-native";
 import AddItem from "./components/AddItem";
 import Header from "./components/Header";
 import ListItem from "./components/ListItem";
 
 export default function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      text: "Milk",
-    },
-    {
-      id: 2,
-      text: "Eggs",
-    },
-    {
-      id: 3,
-      text: "Bread",
-    },
-    {
-      id: 4,
-      text: "Juice",
-    },
-  ]);
+  const [items, setItems] = useState([]);
+
+  // // dummy data
+  // const [items, setItems] = useState([
+  //   {
+  //     id: 1,
+  //     text: "Milk",
+  //   },
+  //   {
+  //     id: 2,
+  //     text: "Eggs",
+  //   },
+  //   {
+  //     id: 3,
+  //     text: "Bread",
+  //   },
+  //   {
+  //     id: 4,
+  //     text: "Juice",
+  //   },
+  // ]);
+
+  // get data from database
+  useEffect(async () => {
+    const result = await fetch("http://localhost:4000/shop/");
+    const data = await result.json();
+    setItems(data);
+  }, []);
 
   const deleteItem = (id) => {
     setItems((prevItems) => {
@@ -33,7 +42,7 @@ export default function App() {
 
   const addItem = (text) => {
     if (!text) {
-      Alert.alert('Error', "Please enter an item", [{text: 'OK'}])
+      Alert.alert("Error", "Please enter an item", [{ text: "OK" }]);
     } else {
       setItems((prevItems) => {
         return [{ id: Math.random(), text }, ...prevItems];
